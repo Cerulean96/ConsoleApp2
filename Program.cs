b05d29781cf27be6a365f7c5d70e1cdc466c4276
Cerulean96 <104350347+Cerulean96@users.noreply.github.com>
2022-05-06 21:21:43 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System;
+
+
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            int a;
+
+            bool reults = int.TryParse(str, out a);
+
+            if (reults)
+            {
+                Console.WriteLine("Операция успешно завершена. Результат операции: " + a);
+            }
+            else
+            {
+                Console.WriteLine("Конвертация не удалась!");
+            }
+        }
+    }
+}
